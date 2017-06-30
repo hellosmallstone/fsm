e99b4538c7d70ea59d076ffe171decf88aca0a88
wuxu <wuxu1103@163.com>
2017-06-30 18:14:48 +0800

    add multiple sources to a destination example

--- examples/simple.go
+++ examples/simple.go
@@ -12,14 +12,42 @@ func main() {
 		"closed",
 		fsm.Events{
 			{Name: "open", Src: []string{"closed"}, Dst: "open"},
-			{Name: "close", Src: []string{"open"}, Dst: "closed"},
+			{Name: "closing", Src: []string{"open"}, Dst: "closing"},
+			{Name: "close", Src: []string{"open", "closing"}, Dst: "closed"},
 		},
 		fsm.Callbacks{},
 	)
 
+    //closed ----> open ----> closing ----> closed
+    fmt.Println("example 1: closed ----> open ----> closing ----> closed")
 	fmt.Println(fsm.Current())
 
 	err := fsm.Event("open")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(fsm.Current())
+
+	err = fsm.Event("closing")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(fsm.Current())
+
+	err = fsm.Event("close")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(fsm.Current())
+
+	//closed ----> open ---->closed
+	fmt.Println("example 2: closed ----> open ----> closed")
+	fmt.Println(fsm.Current())
+
+	err = fsm.Event("open")
 	if err != nil {
 		fmt.Println(err)
 	}
